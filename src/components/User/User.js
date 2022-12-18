cab1f0be6a98261466bd92839b3c6bd01f2da4ec
unknown <kamilabbas.info@gmail.com>
2022-12-18 09:14:47 +0400

    changes

--- src/components/User/User.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import './User.css'
-
-export default class User extends Component {
-  render() {
-    return <div className="user-container">Nərmin Məmmədova</div>;
-  }
-}
